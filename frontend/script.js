667ae312ae752b995d97b425be6107c7510acf37
Apurva2911 <apurvak2911@gmail.com>
2026-02-03 12:36:26 +0530

    Connect frontend to running backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 async function callAPI() {
   const response = await fetch(
-    "backenddeb-h0fnfbg9esc0f4h3.centralindia-01.azurewebsites.net"
+    "https://backenddeb-h0fnfbg9esc0f4h3.centralindia-01.azurewebsites.net/api/hello"
   );
   const data = await response.json();
   document.getElementById("result").innerText =
